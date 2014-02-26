8f3df209ead25f8a2770ae5472938c35171007b8
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2014-02-27 00:29:29 +0100

    Do not duplicate the signature of a standard set

--- glical.mli
+++ glical.mli
@@ -7,36 +7,8 @@
 
 include module type of Glical_kernel
 
-(** SSet = Set.Make(String) *)
-module SSet :
-  sig
-    type elt = String.t
-    type t = Set.Make(String).t
-    val empty : t
-    val is_empty : t -> bool
-    val mem : elt -> t -> bool
-    val add : elt -> t -> t
-    val singleton : elt -> t
-    val remove : elt -> t -> t
-    val union : t -> t -> t
-    val inter : t -> t -> t
-    val diff : t -> t -> t
-    val compare : t -> t -> int
-    val equal : t -> t -> bool
-    val subset : t -> t -> bool
-    val iter : (elt -> unit) -> t -> unit
-    val fold : (elt -> 'a -> 'a) -> t -> 'a -> 'a
-    val for_all : (elt -> bool) -> t -> bool
-    val exists : (elt -> bool) -> t -> bool
-    val filter : (elt -> bool) -> t -> t
-    val partition : (elt -> bool) -> t -> t * t
-    val cardinal : t -> int
-    val elements : t -> elt list
-    val min_elt : t -> elt
-    val max_elt : t -> elt
-    val choose : t -> elt
-    val split : elt -> t -> t * bool * t
-  end
+module SSet : Set.S with type elt = String.t
+(** Set of strings. *)
 
 (** [channel_contents ic] eats all contents of [ic] and returns it as
     a string. Beware: if the contents is very big, it might fail,
